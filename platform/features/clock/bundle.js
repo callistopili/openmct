dd4cfa8f45583dc80f6981a0a5e3dfd29ebebddb
Mandlik, Nikhil K. (ARC-TI)[KBR Wyle Services, LLC] <nikhil.k.mandlik@nasa.gov>
2021-06-02 15:23:11 -0700

    fixed Timer, default timerFormat value

--- platform/features/clock/bundle.js
+++ platform/features/clock/bundle.js
@@ -295,7 +295,7 @@ define([
                             }
                         ],
                         "model": {
-                            "timerFormat": "DDD hh:mm:ss"
+                            "timerFormat": "long"
                         }
                     }
                 ],
